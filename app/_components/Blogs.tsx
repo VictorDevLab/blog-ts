37300352b941782ae2646fb4ca3e02711210a16d
Victor Koome <koomev786@gmail.com>
2025-12-10 15:01:16 +0300

    feat: implement the blogs main page

--- app/_components/Blogs.tsx
+++ app/_components/Blogs.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 interface Blog {
     id: number;
     title: string;
@@ -9,21 +10,27 @@ type BlogsProps = {
     blogs: Blog[];
 }
 
-function Blogs({blogs}: BlogsProps) {
+function Blogs({ blogs }: BlogsProps) {
     return (
-        <div className="mt-16 grid grid-cols-3 gap-1">{
-            blogs.map((blog) => (
-                <div key={blog.id} className="bg-white overflow-hidden border-1 border-amber-700">
-                    {blog.imageUrl && (
-                        <img src={blog.imageUrl} alt={blog.title} className="w-full h-48 object-cover" />
-                    )}
-                    <div className="p-4">
-                        <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
-                        <p className="text-gray-600">{blog.summary}</p>
+        <>
+            <div className="mt-16 grid md:grid-cols-3 sm:grid-cols-1 gap-1">{
+                blogs.map((blog) => (
+                    <div key={blog.id} className="bg-white overflow-hidden border-1 border-amber-700 hover:translate-y-[-8px] transition-transform cursor-pointer">
+                        {blog.imageUrl && (
+                            <img src={blog.imageUrl} alt={blog.title} className="w-full h-48 object-cover" />
+                        )}
+                        <div className="p-4">
+                            <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
+                            <p className="text-gray-600">{blog.summary}</p>
+                        </div>
                     </div>
-                </div>
-            ))
-        }</div>
+                ))
+            }
+            </div>
+            <div className="flex justify-center align-middle my-10">
+                <button className="border bg-black text-white px-10 py-3 hover:translate-y-1 transition-transform cursor-pointer"><Link href="/blogs">Load More</Link></button>
+            </div>
+        </>
     )
 }
 
